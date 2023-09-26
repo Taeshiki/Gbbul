067e479443de1e161058caf5626d88bbee7472e0
jooYeon <conn0204@naver.com>
2023-09-26 17:34:53 +0900

    [FEAT]: vocaView panGesture 추가

--- Gbbul/Gbbul/StudyViewController.swift
+++ Gbbul/Gbbul/StudyViewController.swift
@@ -22,7 +22,7 @@ class StudyViewController: BaseViewController {
     }()
     
     private lazy var vocaView: UIView = {
-       let view = UIView()
+        let view = UIView()
         // 색깔 어떻게 할까..?? 버튼이랑 같은 느낌으로 갈까..
         view.backgroundColor = Palette.pink.getColor()
         return view
@@ -71,6 +71,62 @@ class StudyViewController: BaseViewController {
         
         vocaLabel.snp.makeConstraints { make in
             make.centerX.centerY.equalToSuperview()
+        }
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        vocaView.addGestureRecognizer(gesture)
+    }
+    
+    
+    @objc func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+        // 움직일 view = vocaView
+        guard let voca = sender.view else { return }
+
+        // 지정된 뷰를 기준으로 사용자가 드래그한 거리를 반환
+        let point = sender.translation(in: voca)
+        
+        // 부모뷰의 중심
+        let centerOfSuperView = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        
+        // vocaView를 움직일 때마다 부모뷰의 중심을 통해, 이동 된 vocaView의 중심을 구함
+        voca.center = CGPoint(x: centerOfSuperView.x + point.x, y: centerOfSuperView.y + point.y)
+        
+        switch sender.state {
+            
+            // 제스처 멈췄을 때, 숫자값은 테스트를 통해 적당한 값을 임의로 설정
+        case .ended:
+            // 오른쪽
+            if (voca.center.x) > 20 {
+                UIView.animate(withDuration: 0.2) {
+                    voca.center = CGPoint(x: voca.center .x,
+                                          y: voca.center .y)
+                    voca.alpha = 0
+                }
+                
+            // 왼쪽
+            } else if voca.center.x < -20 {
+                UIView.animate(withDuration: 0.2) {
+                    voca.center = CGPoint(x: voca.center .x,
+                                          y: voca.center .y)
+                    voca.alpha = 0
+                }
+
+            // 위의 값이 아닌 곳에서 제스처를 멈췄을 때는 view를 중앙으로 다시 이동
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    voca.transform = .identity
+                    voca.center = CGPoint(x: self.view.frame.width / 2,
+                                          y: self.view.frame.height / 2)
+                }
+            }
+
+        // 제스처가 움직이고 있을 때, 비스듬하게
+        case .changed:
+            let rotation = point.x / view.frame.width
+            voca.transform = CGAffineTransform(rotationAngle: -rotation)
+            
+        default:
+            break
         }
     }
 }
